709b3ca01e1bbf6f746c4e670551b33011ce0c5c
victor <victorcuru@gmail.com>
2022-01-06 18:59:04 -0300

    Introdução ao .Net e Angular

---
+++ Server/src/ProEventos.Api/Controllers/EventosController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ProEventos.Api.Data;
+using ProEventos.Api.Models;
+
+namespace ProEventos.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventosController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public EventosController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _context.Eventos;
+        }
+
+        [HttpGet("{id}")]
+        public Evento GetById(int id)
+        {
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
+        }
+
+    }
+}
